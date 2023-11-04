001d11ab2b52ee3232b18cc551de15e8bcd02670
程一荣 <97796169+Cheng-lian@users.noreply.github.com>
2023-11-04 16:41:22 +0800

    app_2.py

--- app_2.py
+++ app_2.py
@@ -1,33 +1,39 @@
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.header import Header
 from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
 
-def send_email(sender_email, recipient_email, mail_content):
-    msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = recipient_email
-    msg['Subject'] = Header("dasdas",'utf-8')
+# smtplib模块主要负责发送邮件：是一个发送邮件的动作，连接邮箱服务器，登录邮箱，发送邮件（有发件人，收信人，邮件内容）。
+# email模块主要负责构造邮件：指的是邮箱页面显示的一些构造，如发件人，收件人，主题，正文，附件等。
 
-    #qq邮箱smtp服务器
-    host_server = 'smtp.qq.com'  
-
+def send(sender_qq,receiver,mail_title,mail_content):
+    host_server = 'smtp.qq.com'  #qq邮箱smtp服务器
+    
     #授权码
     pwd = 'hmqjtysogqskhcid'
-    # 连接到SMTP服务器并发送邮件
+
+    # 初始化一个邮件主体
+    msg = MIMEMultipart()
+    msg["Subject"] = Header(mail_title,'utf-8')
+    msg["From"] = sender_qq
+    # msg["To"] = Header("测试邮箱",'utf-8')
+    msg['To'] = receiver
+
+    # 邮件正文内容
     try:
         msg.attach(MIMEText(mail_content,'plain','utf-8'))
 
         smtp = SMTP_SSL(host_server) # ssl登录
 
-        smtp.login(sender_email,pwd)
+        smtp.login(sender_qq,pwd)
 
         # sendmail(from_addr,to_addrs,msg,...):
         # from_addr:邮件发送者地址
         # to_addrs:邮件接收者地址。字符串列表['接收地址1','接收地址2','接收地址3',...]或'接收地址'
         # msg：发送消息：邮件内容。一般是msg.as_string():as_string()是将msg(MIMEText对象或者MIMEMultipart对象)变为str。
+        smtp.sendmail(sender_qq,receiver,msg.as_string())
 
-        smtp.sendmail(sender_email,recipient_email,msg.as_string())
+        # quit():用于结束SMTP会话。
         smtp.quit()
     except Exception as e:
         print(f"邮件发送失败: {e}")
